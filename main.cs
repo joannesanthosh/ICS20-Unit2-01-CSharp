95f6d70c6d226220b2e99abf7013aff8fa03cdec
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-04 18:51:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+// This file displays Hello World
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
